6b1a1409f28826eb4b2c4955ea9f074359b8fe1a
Chaudhary Aditya <chaudharyaditya@192.168.1.49>
2025-02-21 11:27:45 +0530

    Resume updated

--- main.js
+++ main.js
@@ -1,17 +1,32 @@
-     let hamburger = document.querySelector('.hamburger')
-        let phoneMenu = document.querySelector('.header-menu')
-        let lin1 = document.querySelector('.line1')
-        let lin2 = document.querySelector('.line2')
-        let lin3 = document.querySelector('.line3')
-        hamburger.addEventListener('click', () => {
-            phoneMenu.classList.toggle('active');
-            lin1.classList.toggle('active')
-            lin2.classList.toggle('active')
-            lin3.classList.toggle('active')
-        })
-        window.addEventListener('scroll',()=>{
-            phoneMenu.classList.remove('active');
-            lin1.classList.remove('active')
-            lin2.classList.remove('active')
-            lin3.classList.remove('active')
-        })
+let hamburger = document.querySelector('.hamburger')
+let phoneMenu = document.querySelector('.header-menu')
+let lin1 = document.querySelector('.line1')
+let lin2 = document.querySelector('.line2')
+let lin3 = document.querySelector('.line3')
+hamburger.addEventListener('click', () => {
+    phoneMenu.classList.toggle('active');
+    lin1.classList.toggle('active')
+    lin2.classList.toggle('active')
+    lin3.classList.toggle('active')
+})
+window.addEventListener('scroll', () => {
+    phoneMenu.classList.remove('active');
+    lin1.classList.remove('active')
+    lin2.classList.remove('active')
+    lin3.classList.remove('active')
+})
+
+
+    window.onload = function () {
+        var link = document.createElement('a');
+        link.href = './Aditya_resume.pdf';
+        // link.download = 'Aditya_CV.pdf';
+        link.download = 'Aditya_resume.pdf';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    };
+
+
+
+
